2da4d76e967fc802c71bf87d0f421a513fdd3e5c
Infinite-Unexpired-Love <jxpxxly@126.com>
2023-05-17 19:30:09 +0800

    Final version

--- lib/posts.ts
+++ lib/posts.ts
@@ -23,7 +23,7 @@ export function getSortedPostsData() {
         // Combine the data with the id
         return {
             id,
-            ...matterResult.data,
+            ...(matterResult.data as { date: string, title: string }),
         };
     });
     // Sort posts by date
@@ -47,7 +47,7 @@ export function getAllPostsId() {
     })
 }
 
-export async function getPostDataById(id) {
+export async function getPostDataById(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`);
     const fileContents = fs.readFileSync(fullPath, 'utf-8');
     const matterResult = matter(fileContents);
@@ -58,6 +58,6 @@ export async function getPostDataById(id) {
     return {
         id,
         contentHtml,
-        ...matterResult.data
+        ...(matterResult.data as { date: string, title: string })
     }
 }
